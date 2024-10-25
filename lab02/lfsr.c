023195a479ebf0aae3831c1c3eea2fbf808f5c9d
Chris <2293611667@qq.com>
2024-10-26 01:15:39 +0800

    Finish the lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,8 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t newBit = (*reg & 1) ^ (*reg >> 2 & 1) ^ ( *reg >> 3 & 1) ^ (*reg >> 5 & 1);
+    *reg >>= 1;
+    *reg |= newBit << 15;
 }
 
