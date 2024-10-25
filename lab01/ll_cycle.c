e5374364e36f8a7ce2edc9ad76c72137a02cca0a
Chris <2293611667@qq.com>
2024-10-25 23:24:11 +0800

    Finish the lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,16 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if(!head || !head->next) return 0;
+    node* tortoise = head->next->next ; 
+    node* hare  = head->next;
+    while (tortoise && hare)
+    {
+        if(tortoise == hare ) return 1;
+        if(! tortoise->next) return 0;
+        tortoise = tortoise->next->next ;
+        hare  = hare->next;
+    }
+    
     return 0;
 }
